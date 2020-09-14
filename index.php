bd226144bac5ea723c72fa6a06d24fcde116407d
vunf1 <jokass.workplace@gmail.com>
2020-09-14 21:36:54 +0100

    test heroku

--- index.php
+++ index.php
@@ -1,59 +1,64 @@
 <!DOCTYPE html>
-<html lang="en"   style="background-color: white;"  >
+<html lang="en" style="background-color: white;">
 
-  <head>
+<head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="CV">
-    <meta name="author" content="Maia">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="CV">
+  <meta name="author" content="Maia">
 
-    <title>Portfolio - João Maia</title>
+  <title>Portfolio - João Maia</title>
 
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom fonts for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-    <link href="vendor/font-awesome/css/all.css" rel="stylesheet">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900"
+    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+    rel="stylesheet">
+  <link href="vendor/font-awesome/css/all.css" rel="stylesheet">
 
-    <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
+  <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
+  <link rel="stylesheet"
+    href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
 
-    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+  <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="./css/resume.min.css" rel="stylesheet">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link href="./css/custom.css" rel="stylesheet">
-    <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+  <!-- Custom styles for this template -->
+  <link href="./css/resume.min.css" rel="stylesheet">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link href="./css/custom.css" rel="stylesheet">
+  <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
 
-    <!-- Allertify styles -->
-    
-<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/alertify.min.js"></script>
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/css/alertify.min.css"/>
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/css/themes/bootstrap.min.css"/>
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/css/themes/default.min.css"/>
+  <!-- Allertify styles -->
 
-  </head>
+  <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/alertify.min.js"></script>
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/css/alertify.min.css" />
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/css/themes/bootstrap.min.css" />
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/css/themes/default.min.css" />
 
-  <body id="page-top" >
+</head>
 
-   
-        <img id="loading" src="img/loading/Preloader_3.gif" >
+<body id="page-top">
 
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top navbarColor " id="sideNav">
-      
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">
-        <a class="d-lg-none" style="display: contents;">
-          <img class="transLogo topFlag" id="pt" style="width: 10%;height:10%; " src="img/pt_logo.png">
-          <span class="d-block d-lg-none  navTitleTXT"  style="color: white;">Portfolio</span>
-          <img class="transLogo topFlag" id="eng" style="width: 10%;height:10%; " src="img/eng_logo.png">
-          <button  class="fas fa-bars navbar-toggler menuTri" id="off"  data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"></button>
-        </a>
-        
+  <img id="loading" src="img/loading/Preloader_3.gif">
 
-        <!--  
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top navbarColor " id="sideNav">
+
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <a class="d-lg-none" style="display: contents;">
+        <img class="transLogo topFlag" id="pt" style="width: 10%;height:10%; " src="img/pt_logo.png">
+        <span class="d-block d-lg-none  navTitleTXT" style="color: white;">Portfolio</span>
+        <img class="transLogo topFlag" id="eng" style="width: 10%;height:10%; " src="img/eng_logo.png">
+        <button class="fas fa-bars navbar-toggler menuTri" id="off" data-toggle="collapse"
+          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+          aria-label="Toggle navigation"></button>
+      </a>
+
+
+      <!--  
         <a target="_blank" class="d-block d-lg-none topIcon"  href="https://github.com/vunf1/">
           <span class="fa-stack fa-lg">
             <i class="fas fa-circle fa-stack-2x"></i>
@@ -67,99 +72,102 @@
             <i class="fas fa-linkedin fa-stack-1x fa-inverse"></i>
           </span>
         </a>-->
-        <!-- Title Alternative <span class="d-block d-lg-none">Portfolio</span> -->
+      <!-- Title Alternative <span class="d-block d-lg-none">Portfolio</span> -->
 
-      </a>
+    </a>
 
-      <style>
-        .fa.fa-bars {
-          
-            opacity: 0.8;      
-            color: rgba(22, 51, 22, 0.75);   
-                    
-            background-color: Transparent;
-            background-repeat:no-repeat;
-            border: none;
-            outline:none;
-        }
+    <style>
+      .fa.fa-bars {
 
-        .transLogo,#profileIMG{
-          visibility:hidden;
-        }
-        #text_inter:first-child:first-letter {
-          color: rgb(0, 0, 0);
-          float: left;
-          font-family: Georgia;
-          font-size: 90px;
-          line-height: 60px;
-          padding-top: 4px;
-          padding-right: 8px;
-          padding-left: 3px;
-        }
-      </style>
+        opacity: 0.8;
+        color: rgba(22, 51, 22, 0.75);
 
-      <div class="collapse navbar-collapse " id="navbarSupportedContent">
-        <ul class="navbar-nav" style="text-align: center">
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger profTop" id="g_about" >About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger loadDiv" id="g_experience">Experience</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger loadDiv" id="g_education">Education</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger loadDiv" id="g_skills">Skills</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger loadDiv" id="g_interests">Interests</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger loadDiv" id="g_awards">Certifications</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger loadDiv" id="g_project">Projects</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger">
-              <img class="transLogo " id="pt" src="img/pt_logo.png"><span> </span>
-              <img class="transLogo" id="eng" src="img/eng_logo.png"></a>
-          </li>
-          <span class="button-checkbox">
-            
-              <h3 id="darkMode">Dark Mode <a  style="outline: none;"  class="darkModebtn text-white " id="OFF"  > OFF</a></h3> 
-          </span>
-          
-          <li >
-            
-            <img title="Taking a Coffee while you see all" style="width:70%;height:70%;" src="img/root/blow_coofee_human.gif">
-          
-          </li>
-        </ul>
-      </div>
-    </nav>
-<div class="container-fluid  caixa" style="margin-top: 10%!important;">
+        background-color: Transparent;
+        background-repeat: no-repeat;
+        border: none;
+        outline: none;
+      }
 
+      .transLogo,
+      #profileIMG {
+        visibility: hidden;
+      }
 
-</div>
+      #text_inter:first-child:first-letter {
+        color: rgb(0, 0, 0);
+        float: left;
+        font-family: Georgia;
+        font-size: 90px;
+        line-height: 60px;
+        padding-top: 4px;
+        padding-right: 8px;
+        padding-left: 3px;
+      }
+    </style>
 
- <!-- Bootstrap core JavaScript -->
-    <script src="./vendor/jquery/jquery.min.js"></script>
-    <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav" style="text-align: center">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger profTop" id="g_about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger loadDiv" id="g_experience">Experience</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger loadDiv" id="g_education">Education</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger loadDiv" id="g_skills">Skills</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger loadDiv" id="g_interests">Interests</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger loadDiv" id="g_awards">Certifications</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger loadDiv" id="g_project">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger">
+            <img class="transLogo " id="pt" src="img/pt_logo.png"><span> </span>
+            <img class="transLogo" id="eng" src="img/eng_logo.png"></a>
+        </li>
+        <span class="button-checkbox">
 
-    <!-- Plugin JavaScript -->
-    <script src="./vendor/jquery-easing/jquery.easing.min.js"></script>
+          <h3 id="darkMode">Dark Mode <a style="outline: none;" class="darkModebtn text-white " id="OFF"> OFF</a></h3>
+        </span>
+
+        <li>
+
+          <img title="Taking a Coffee while you see all" style="width:70%;height:70%;"
+            src="img/root/blow_coofee_human.gif">
+
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container-fluid  caixa" style="margin-top: 10%!important;">
 
 
-    <!-- Custom scripts for this template https://alertifyjs.org-->
-<script src="https://cdn.rawgit.com/alertifyjs/alertify.js/v1.0.10/dist/js/alertify.js"></script>
+  </div>
 
-<script src="./js/alertify1.0.10.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script src="./vendor/jquery/jquery.min.js"></script>
+  <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <script src="./js/modernizr2.8.2.js"></script>
+  <!-- Plugin JavaScript -->
+  <script src="./vendor/jquery-easing/jquery.easing.min.js"></script>
 
-    <script src="./js/resume.min.js"></script>
-  </body>
+
+  <!-- Custom scripts for this template https://alertifyjs.org-->
+  <script src="https://cdn.rawgit.com/alertifyjs/alertify.js/v1.0.10/dist/js/alertify.js"></script>
+
+  <script src="./js/alertify1.0.10.js"></script>
+
+  <script src="./js/modernizr2.8.2.js"></script>
+
+  <script src="./js/resume.min.js"></script>
+</body>
 
 </html>
